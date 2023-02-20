4ba161d2b73fa006f0e860d7e820fcae61752fc9
Melvin Bulan <melvin.bulan000@gmail.com>
2023-02-20 21:32:17 +0800

    Add work section card layouts

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -4,7 +4,16 @@ import '../App.css'
 const WorkSection = ({workWrapper}) => {
   return (
     <div className={workWrapper ? 'normColor' : 'dark'}>
-        
+       <div className="normCont">
+         <div className="innWrapper cont">
+            <div className="left">
+                <div className="imgs"></div>
+            </div>
+            <div className="right">
+                Test
+            </div>
+        </div>  
+       </div>
     </div>
   )
 }
