63766c186359e44ba72b0e67cd2b3baa5a1ddf0e
Melvin Bulan <melvin.bulan000@gmail.com>
2023-02-20 16:56:40 +0800

    Add card components

---
+++ src/components/OtherProjects.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../App.css';
+
+const OtherProjects = () => {
+  return (
+    <div className='opWrapper'>
+        <h1 className='otherProj'>Other Projects</h1>
+        <div className="containerWrapper">
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+        </div>
+   </div>
+  )
+}
+
+export default OtherProjects
