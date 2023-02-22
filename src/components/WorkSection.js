c2f568a221a3cfd2462aee0a004dd9639d71b60c
Melvin Bulan <melvin.bulan000@gmail.com>
2023-02-22 08:58:30 +0800

    Add placeholder image

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -5,14 +5,16 @@ const WorkSection = ({lightBg, titleShown, reverseCard, projImg}) => {
 
   return (
     <div className={lightBg ? 'normColor' : 'dark'}>
-       
+       <div className= "title" style={{display: titleShown ? "block" : "none"}}>
+          <h1>Featured Works</h1>
+       </div>
        <div className="normCont">
          <div className={reverseCard ? "innWrapper" : "rev"}>
             <div className="left">
                
             </div>
             <div className="right">
-             
+            <img className="imgBanner" src={projImg} alt="Project 1" />
             </div>
         </div>  
        </div>
