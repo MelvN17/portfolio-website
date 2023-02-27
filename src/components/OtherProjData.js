843e24969deea453dae28bce6fcb44dc4f508b2b
Melvin Bulan <melvin.bulan000@gmail.com>
2023-02-27 12:35:54 +0800

    Add data handler

---
+++ src/components/OtherProjData.js
@@ -0,0 +1,61 @@
+import myImageI from '../img/proj1.png'
+//imageSrc, imageAlt, title, tech, info, liveLink, githubLink
+export const otherProjOne = {
+    imageSrc: myImageI,
+    imageAlt: "Image",
+    title: "Title",
+    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum necessitatibus rem ab alias. Ratione, suscipit.",
+    liveLink: "/#", 
+    githubLink: "/#"
+};
+
+export const otherProjTwo = {
+    imageSrc: myImageI,
+    imageAlt: "Image",
+    title: "Title",
+    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum necessitatibus rem ab alias. Ratione, suscipit.",
+    liveLink: "/#", 
+    githubLink: "/#"
+};
+
+export const otherProjThree = {
+    imageSrc: myImageI,
+    imageAlt: "Image",
+    title: "Title",
+    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum necessitatibus rem ab alias. Ratione, suscipit.",
+    liveLink: "/#", 
+    githubLink: "/#"
+};
+
+export const otherProjFour = {
+    imageSrc: myImageI,
+    imageAlt: "Image",
+    title: "Title",
+    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum necessitatibus rem ab alias. Ratione, suscipit.",
+    liveLink: "/#", 
+    githubLink: "/#"
+};
+
+export const otherProjFive = {
+    imageSrc: myImageI,
+    imageAlt: "Image",
+    title: "Title",
+    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum necessitatibus rem ab alias. Ratione, suscipit.",
+    liveLink: "/#", 
+    githubLink: "/#"
+};
+
+export const otherProjSix = {
+    imageSrc: myImageI,
+    imageAlt: "Image",
+    title: "Title",
+    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum necessitatibus rem ab alias. Ratione, suscipit.",
+    liveLink: "/#", 
+    githubLink: "/#"
+};
