b2004411ee7c5f172e131a53d7f148eeb10b3edc
Melvin Bulan <melvin.bulan000@gmail.com>
2023-03-02 00:11:31 +0800

    Update work section design and code

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -4,19 +4,19 @@ import '../App.css'
 const WorkSection = ({lightBg, titleShown, reverseCard, projImg}) => {
 
   return (
-    <div className={lightBg ? 'normColor' : 'dark'}>
+    <section className="workContainer">
        <div className= "title" style={{display: titleShown ? "block" : "none"}}>
           <h1>Featured Works</h1>
        </div>
        <div className="normCont">
-         <div className={reverseCard ? "innWrapper" : "rev"}>
+         <div className={reverseCard ? "normal" : "reverse"}>
             <div className="left">
-               
+               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus voluptates impedit adipisci quas dolorum quos eum praesentium perferendis! Odio minus vero porro accusantium obcaecati aspernatur.</p>
             </div>
             <img className="right" src={projImg} alt="Project 1" />
         </div>  
        </div>
-    </div>
+    </section>
   )
 }
 
