67fd49580d31bc0ed79688d0b6ea839b3207f745
Melvin Bulan <melvin.bulan000@gmail.com>
2023-03-02 14:46:37 +0800

    Update files

--- src/components/workData.js
+++ src/components/workData.js
@@ -4,19 +4,19 @@ export const homeObjOne = {
     projImg: myImage,
     titleShown: true,
     title: "Title",
-    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
-    info: "lorem ipsum something something",
+    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem', 'Lorem'],
+    info: "lorem ipsum something somethinglorem ipsum something somethinglorem ipsum something somethinglorem ipsum something something",
     liveLink: "/#", 
     githubLink: "/#",
 };
 
 export const homeObjTwo = {
-    reverseCard: true,
+    reverseCard: false,
     projImg: myImage,
     titleShown: false,
     title: "Title",
     techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
-    info: "testing",
+    info: "lorem ipsum something somethinglm ipsum something somethinglorem ipsum something something",
     liveLink: "/#", 
     githubLink: "/#",
 };
@@ -27,18 +27,18 @@ export const homeObjThree = {
     titleShown: false,
     title: "Title",
     techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
-    info: "testing",
+    info: "lorem ipsum something somethinging somethinging somethinging something",
     liveLink: "/#", 
     githubLink: "/#",
 };
 
 export const homeObjFour = {
-    reverseCard: true,
+    reverseCard: false,
     projImg: myImage,
     titleShown: false,
     title: "Title",
     techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
-    info: "testing",
+    info: "lorem ipsum something somethingloremsomethingloremsomethinglorem ipsum something something",
     liveLink: "/#", 
     githubLink: "/#",
 };
