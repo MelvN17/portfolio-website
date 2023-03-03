ee8836e6a4cf610e9b94e5d46acc29daa4874411
Melvin Bulan <melvin.bulan000@gmail.com>
2023-03-04 01:52:23 +0800

    Add other project data

--- src/components/OtherProjData.js
+++ src/components/OtherProjData.js
@@ -1,13 +1,14 @@
+import otherProj1 from '../img/otherProj/otherProj1.png'
 import myImageI from '../img/proj1.png'
 //imageSrc, imageAlt, title, tech, info, liveLink, githubLink
 export const otherProjOne = {
-    imageSrc: myImageI,
+    imageSrc: otherProj1,
     imageAlt: "Image",
-    title: "Title",
-    techStack: ['Lorem', 'Lorem', 'Lorem', 'Lorem'],
-    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum necessitatibus rem ab alias. Ratione, suscipit.",
-    liveLink: "/#", 
-    githubLink: "/#"
+    title: "Dad Jokes",
+    techStack: ['React', 'SCSS', 'API Fetching', 'Vite'],
+    info: "A react-app using vite with an integration of a simple fetch api and styled using SCSS. Happy Father's day!",
+    liveLink: "https://dad-jokes-generator-two.vercel.app/", 
+    githubLink: "https://github.com/MelvN17/dad-jokes-generator"
 };
 
 export const otherProjTwo = {
